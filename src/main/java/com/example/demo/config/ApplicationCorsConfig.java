402c76ba6745453d02eb4ec2b0871e5e90b1024d
brianhanna <bhanna1693@gmail.com>
2020-08-31 19:44:00 -0400

    update cors config and angular.json build output path

---
+++ src/main/java/com/example/demo/config/ApplicationCorsConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApplicationCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200");
+    }
+}
+
