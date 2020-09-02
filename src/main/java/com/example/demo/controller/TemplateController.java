53d295a271f7298630c8ac11ae5c5cc67e846ae0
brianhanna <bhanna1693@gmail.com>
2020-09-02 09:41:43 -0400

    update TemplateController

--- src/main/java/com/example/demo/controller/TemplateController.java
+++ src/main/java/com/example/demo/controller/TemplateController.java
@@ -1,29 +1,19 @@
 package com.example.demo.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TemplateController {
-    private boolean hasLoggedIn = false;
 
     @GetMapping("/login")
     public String getLoginView() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated() && !(authentication.getPrincipal()).equals("anonymousUser")) {
-            return "redirect:/";
-        } else {
-            // not logged in, send to login page.
-            return "login";
-        }
-
-
+        // not logged in, send to login page.
+        return "login";
     }
 
     @GetMapping("/index")
     public String getHomeView() {
-        return "redirect:/";
+        return "/";
     }
 }
