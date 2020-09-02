fd61ee168310c827124e82a7366c53574265b3bb
brianhanna <bhanna1693@gmail.com>
2020-09-02 11:45:30 -0400

    environment.prod.ts; output path adjusted to static

--- src/main/webapp/src/environments/environment.prod.ts
+++ src/main/webapp/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'http://localhost:8080/api/v1',
+  MANAGEMENT_API_URL: 'http://localhost:8080/management/api/v1',
+  BASE_URL: 'http://localhost:8080'
 };
